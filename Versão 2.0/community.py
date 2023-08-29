ddb1903068d27fefe23d109dcdfdbbb530c19ca3
Wolfshohle <112727821+Wolfshohle@users.noreply.github.com>
2023-08-29 16:32:59 -0300

    Update community.py

--- Versão 2.0/community.py
+++ Versão 2.0/community.py
@@ -1,113 +1,107 @@
 from autentic import autentic
-from data import data_community_whit_user
-from data import data_user_whit_community
 
-class Community:
-    #inicia a classe
+class Account:
     def __init__(self):
-        self.communitys = []
+        self.users = []
 
 
-    #cria a comunidade
-    def create_community(self, name, description, admin):
-        new_community = {
-            "name": name,
-            "description": description,
-            "member": [{"username": admin["username"], "access": 0}],
-            "banned":[]
-        }
-
-        self.communitys.append(new_community)
-
-        new_community_for_user = {"name": name}
-        admin["community"].append(new_community_for_user)
+    #cria a conta
+    def create_account(self, name, password, username, email):
+        new_user = {
+                "name": name,
+                "password": password,
+                "username": username,
+                "email": email,
+                "followers": [],
+                "following": [],
+                "community": []
+            }
+        self.users.append(new_user)
 
     
-    #edita o nome da comunidade
-    def edit_name_of_comunity(self, youcommunity, name, new_name, userList):
-        for user in youcommunity["member"]:
-                user_edit = autentic.finduser(user["username"], userList)
-                for community_edit in user_edit["community"]:
-                    if community_edit["name"] == name:
-                        community_edit["name"] = new_name
-                        break
-
-        youcommunity["name"] = new_name
+    #edita o nome da conta
+    def edit_account_name(self, new_name, user):
+        user["name"] = new_name
 
 
-    #edita a descrição da comunidade
-    def edit_description_of_community(self, new_description, youcommunity):
-        new_description = new_description.capitalize()
-        youcommunity["description"] = new_description
+    #edita o username da conta
+    def edit_account_username(self, new_username, user, community):
+        #edita o usernome da conta na lista de seguidores
+        for friend in user["followers"]:
+            friend_in_follower = autentic.finduser(friend["username"], self.users)
+            edityouusername = autentic.finduser(user["username"], friend_in_follower["following"])
+            edityouusername["username"] = new_username
+
+        #edita o username da conta na lista de seguidos
+        for friend in user["following"]:
+            friend_in_follower = autentic.finduser(friend["username"], self.users)
+            edityouusername = autentic.finduser(user["username"], friend_in_follower["followers"])
+            edityouusername["username"] = new_username
+
+        #edita o username da conta na lista de comunidades
+        for community_edit in user["community"]:
+            community_to_edit = autentic.findcommunity(community_edit["name"], community)
+            edityouusername = autentic.finduser(user["username"], community_to_edit["member"])
+            edityouusername["username"] = new_username
+
+        user["username"] = new_username
+
+    
+    #edita a senha
+    def edit_account_password(self, new_password, user):
+        user["password"] = new_password
+
+    
+    #edita o email
+    def edit_account_email(self, new_email, user):
+        user["email"] = new_email
 
 
-    #muda o nível de acesso do usuario
-    def change_member_office(self, admin_user, member_of_community, office):
-        if member_of_community["access"] == 0 or admin_user["access"] > 1:
-            print("Acesso negado")
-            return
-        
-        if office != 0 and office != 1 and office != 2:
-            print("Nível de acesso inválido")
-            return
-        
-        member_of_community["access"] = office
-
-
-    #bane e desbane o usuario
-    def ban_user(self, admin_user, member_of_community, community_moderate, opc, userInuserlist):
-        if member_of_community["access"] == 0 or admin_user["access"] > 1 or admin_user["access"] < 0:
-            print("Acesso negado")
-            return
-        
-        #bane o usuario
-        if opc == 1:
-            remove_community = {"name": community_moderate["name"]}
-            member_of_community["access"] = -1
-            community_moderate["member"].remove(member_of_community)
-            userInuserlist["community"].remove(remove_community)
-            community_moderate["banned"].append(member_of_community)
-            
-        #desbane o usuario
-        if opc == 2:
-            community_moderate["banned"].remove(member_of_community)
-
-
-    #segue a comunidadde
-    def followCommunity(self, userInuserlist, community):
-        new_member = {"username": userInuserlist["username"], "access": 2}
-        new_community = {"name": community["name"]}
-        community["member"].append(new_member)
-        userInuserlist["community"].append(new_community)
+    #segue o usuário
+    def userfollow(self, you, friend):
+        you_follow = {"username": you["username"]}
+        new_friend = {"username": friend["username"]}
+        friend["followers"].append(you_follow)
+        you["following"].append(new_friend)
 
 
     #retira o seguir do usuario
-    def unfollowcommunity(self, userInuserlist, community, userIncommunity):
-        high_access = 3
-        delet_member = autentic.finduser(userInuserlist["username"], community["member"])
-        delet_community = autentic.findcommunity(community["name"], userInuserlist["community"])
-
-        community["member"].remove(delet_member)
-        userInuserlist["community"].remove(delet_community)
-
-        if len(community["member"]) > 0:
-            if userIncommunity["access"] == 0:
-                for new_creator in community["member"]:
-                    if new_creator["access"] < high_access:
-                        new_creator_in_community = new_creator
-                        high_access = new_creator["access"]
-                
-                new_creator_in_community["access"] = 0
-        else:
-            self.communitys.remove(community)
+    def unfollow(self, you, friend):
+        remove_you = {"username": you["username"]}
+        remove_friend = {"username": friend["username"]}
+        friend["followers"].remove(remove_you)
+        you["following"].remove(remove_friend)
 
 
     #deleta a conta
-    def deletcommunity(self, userlist, community):
-        for user in userlist:
-            if autentic.checkcommunityname(community["name"], user["community"]):
-                remove_community = autentic.findcommunity(community["name"], user["community"])
-                print(remove_community)
-                user["community"].remove(remove_community)
+    def delet_account(self, you, communitys):
+        #deleta o usuario da lista de seguidores
+        for friend in you["followers"]:
+            friend_in_follower = autentic.finduser(friend["username"], self.users)
+            remove_user = autentic.finduser(you["username"], friend_in_follower["following"])
+            friend_in_follower["following"].remove(remove_user)
 
-        self.communitys.remove(community)
+        #delta o usuario da lista de seguidos
+        for friend in you["following"]:
+            friend_in_follower = autentic.finduser(friend["username"], self.users)
+            remove_user = autentic.finduser(you["username"], friend_in_follower["followers"])
+            friend_in_follower["followers"].remove(remove_user)
+
+        #deleta o usuario da lista de comunidades
+        for community_remove in you["community"]:
+            community_to_remove = autentic.findcommunity(community_remove["name"], communitys)
+            remove_user_in_community = autentic.finduser(you["username"], community_to_remove["member"])
+            community_to_remove["member"].remove(remove_user_in_community)
+
+            if len(community_to_remove["member"]) > 0:
+                if remove_user_in_community["access"] == 0:
+                    for new_creator in community_to_remove["member"]:
+                        if new_creator["access"] < high_access:
+                            new_creator_in_community = new_creator
+                            high_access = new_creator["access"]
+                
+                new_creator_in_community["access"] = 0
+            else:
+                communitys.remove(community_to_remove)
+
+        self.users.remove(you)
