80a9c68110648c2af05d8ae99a6f64a04b52583c
Wolfshohle <112727821+Wolfshohle@users.noreply.github.com>
2023-09-05 20:14:11 -0300

    Update data.py

--- Versão 2.0/versão2.0/data.py
+++ Versão 2.0/versão2.0/data.py
@@ -1,4 +1,6 @@
 import json
+from account import Account
+from community import Community
 
 class Data:
     def __init__(self, filename):
@@ -12,7 +14,42 @@ class Data:
         except FileNotFoundError:
             return []
         
+    def save_data(self, data):
+        with open(self.filename, 'w') as file:
+            json.dump(data, file, indent=4)
 
+
+class userdata(Data):
+    def __init__(self, filename="Conta.json"):
+        super().__init__(filename)
+
+    def load_data(self):
+        try:
+            with open(self.filename, 'r') as file:
+                data = json.load(file)
+                print("Dados de usuarios carregados com sucesso!")
+                return data
+        except FileNotFoundError:
+            return []
+        
+    def save_data(self, data):
+        with open(self.filename, 'w') as file:
+            json.dump(data, file, indent=4)
+
+
+class communiytdata(Data):
+    def __init__(self,filename="Comunidade.json"):
+        super().__init__(filename)
+
+    def load_data(self):
+        try:
+            with open(self.filename, 'r') as file:
+                data = json.load(file)
+                print("Dados de comunidades carregados com sucesso!")
+                return data
+        except FileNotFoundError:
+            return []
+        
     def save_data(self, data):
         with open(self.filename, 'w') as file:
             json.dump(data, file, indent=4)
